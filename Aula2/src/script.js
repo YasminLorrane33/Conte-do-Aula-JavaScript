2754ee93239ebf36cb35661bf07c9b5913c2465f
yasmin <yasminlorrane.yasmin@gmail.com>
2026-02-25 21:19:43 -0300

    Aula 2 - Continuação de Variáveis

---
+++ Aula2/src/script.js
@@ -0,0 +1,77 @@
+// Exemplos de operador de atribuição
+let x = 100;
+
+//x = x - 5;
+// console.log(x);
+
+//x = x + 5;
+// console.log(x);
+
+// Exemplos de Aritmética em JavaScript
+let soma = 5 + 5 + 4;
+console.log(soma);
+
+// const primeironome = 'Yasmin';
+// const ultimonome = 'Lorrane';
+
+let nomeCompleto = 'Yasmin' + 'Lorrane';
+
+console.log(nomeCompleto);
+
+// Exemplos de tipos em JavaScript
+//String
+let cor = 'Azul';
+let fruta = 'Laranja';
+console.log(cor);
+console.log(fruta);
+
+// Number
+let soma1 = 5 + 5;
+let subtracao = 10 - 3;
+console.log(soma1);
+console.log(subtracao)
+
+// BigInt
+let BigInt = 1234567890123456789012345n;
+let BigInt2 = BigInt(1234567890123456789012345);
+console.log(BigInt);
+console.log(BigInt2);
+
+// Boolean
+let valorVerdadeiro = true;
+let valorFalso = false;
+console.log(valorVerdadeiro);
+console.log(valorFalso);
+
+// Object
+const carro = {
+    marca: 'porshe',
+    modelo: '911 Turbo S',
+    cor: 'Ruby Star Neo',
+}
+
+console.log(carro);
+console.log('A marca do carro é: ' + carro.marca);
+
+//array oneject
+const frutas = 
+
+console.log (frutas);
+
+//Date Object
+const data = new Date;
+
+console.log(data);
+
+//undefined
+let indefinido;
+console.log(indefinido);
+
+//null
+let nulo = null;
+
+console.log(nulo);
+
+//symbol
+let y = Symbol();
+console.log();
